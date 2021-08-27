f0437399e27f2c05c2328c5f9386138f09bec25f
Veikko Aho <vidfinnx@yahoo.com>
2021-08-26 17:33:10 -0700

    added footer

--- src/Footer.js
+++ src/Footer.js
@@ -1,9 +1,34 @@
 import React from 'react'
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/esm/Container';
+import LinkedIn from './images/In.svg'
+import Git from './images/Git.svg'
 
  const Footer = () => {
     return (
         <div>
-            <p>FOOTER PLACEHOLDER</p>    
+            <Navbar  style={{backgroundColor:'#343a40', borderRadius: '2%'}} bg="dark" variant="dark">
+    <Container>
+      <Navbar.Brand style={{fontSize: '20px', fontFamily: 'Digital'}} href="https://www.linkedin.com/in/veikko-aho-3125077a/">
+        <img
+          alt=""
+          src={LinkedIn}
+          width="30"
+          height="30"
+          className="d-inline-block"
+        />{' My Linked In'}
+      </Navbar.Brand>
+      <Navbar.Brand style={{fontSize: '20px', fontFamily: 'Digital'}} href="https://github.com/Vidfinnx">
+        <img
+          alt=""
+          src={Git}
+          width="30"
+          height="30"
+          className="d-inline-block"
+        />{' My Github'}
+      </Navbar.Brand>
+    </Container>
+  </Navbar> 
         </div>
     )
 }
