fd82d4d5a2a14ecca43b97bccab2b19207e3e608
Veikko Aho <Vidfinnx@yahoo.com>
2021-12-01 10:36:21 -0800

    added skills to about-me section

--- src/Aboutme.js
+++ src/Aboutme.js
@@ -12,8 +12,12 @@ function Aboutme() {
             <Col style={{fontFamily: 'Digital', fontSize: '30px', fontColor: 'cornflowerblue'}}>About Me</Col>
             <Image src={AhoBG} fluid />
                 {/* <Row> */}
+                <Col style={{borderRadius: '20px', fontFamily: 'Digital', backgroundColor: 'black', color: 'Limegreen'}}><span style={{color: 'yellow'}}>Knowledgeable In:</span> React, Javascript, Node, GraphQL, Jquery, React-Native, Mongo-DB, MySQL, HandlebarsJS, HTML, CSS </Col>
+                <br></br>
                     <Col style={{fontFamily: 'Digital'}}>My name is Veikko Aho,  translated it means brother in the Finnish Language. I am a new Web Developer looking to further my experience and education in any up and coming company or business.</Col>
+                    <br></br>
 <Col style={{fontFamily: 'Digital', color:'red'}}>I am a United States Air Force Veteran, and I have spent the past 14 years as a Paramedic and Volunteer Firefighter. My time in the as a Paramedic and in the Military has given me a drive for excellence a unique ability to adapt to any situation wrapped in compassion and the desire to help wherever possible. </Col>
+<br></br>
 <Col style={{fontFamily: 'Digital', color:'limegreen'}}>Please feel free to contact me, you can find my information above in the drop down menu under Contact-Me. </Col>
                 {/* </Row> */}
             </Container>
